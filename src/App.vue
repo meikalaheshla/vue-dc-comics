717d7852cc444baa21a7b456ba343ecce56d2c74
meikalaheshla <fausto.boshoff@gmail.com>
2022-07-22 18:10:13 +0200

    prepared workspace

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,20 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <TheHeader />
+
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  name: 'DcComics',
+
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import "./assets/sass/style.scss";
 </style>
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <TheHeader />
+
+
+    </div>
+</template>
+
+<script>
+
+
+export default {
+    name: 'BaseHeader',
+
+};
+</script>
+
+<style lang="scss">
+</style>
